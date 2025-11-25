557337535135505584906e91e6ea9924471fe594
Samindra Poudel <poudelsamundra162@gmail.com>
2025-11-25 10:13:50 +0530

    applied insert operation

--- mongo/index.js
+++ mongo/index.js
@@ -11,7 +11,22 @@ client.connect("mongodb://127.0.0.1:27017")
 })
 .catch((err) => console.error("Mongo connect error:", err));
 app.get('/',(req,res)=>res.json({ok:"connected"}));
-app.get("/insert",(req,res)=>{
-    
-})
+app.get("/insertone",(req,res)=>{
+    dbinstance.collection("User").insertOne({name:"xtrem",roll:"1234",sec:"I"})
+    .then(data=>console.log("Data Inserted: ",data))
+    .catch(err=>{
+        console.log("Error insrting data: ",err)
+        res.send("Please try again");
+    })
+    res.json({ok:true});
+});
+app.get("/insertmany",(req,res)=>{
+    dbinstance.collection("User").insertMany([{name:"xtrem",roll:"1234",sec:"I"},{name:"bull",roll:"12",sec:"I"}])
+    .then(data=>console.log("Data Inserted: ",data))
+    .catch(err=>{
+        console.log("Error insrting data: ",err)
+        res.send("Please try again");
+    })
+    res.json({ok:true});
+});
 app.listen(3000);
